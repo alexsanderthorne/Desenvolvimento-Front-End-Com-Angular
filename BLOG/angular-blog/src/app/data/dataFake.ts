a3339c8523c5e1d56898e79610d2b18558e2846a
alexsanderthorne <alexsanderthorne@gmail.com>
2024-03-15 11:12:28 -0300

    melhorias no layout do blog

--- BLOG/angular-blog/src/app/data/dataFake.ts
+++ BLOG/angular-blog/src/app/data/dataFake.ts
@@ -1,38 +1,52 @@
 export const dataFake = [
   {
-    "id":"1",
+    "id": "1",
     "title": "NOVO HOMEM DE FERRO EM 3D",
-    "description": "marvel anuncia um novo filme do homem de ferro, confira",
-    "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/7F51FA9F6CBD9F0C9B1394B1CC0A6A842D07091318674E234CD33CBF7C28CDC3/scale?width=1200&aspectRatio=1.78&format=jpeg"
+    "description": "1º de abril – Homem de Ferro 4 é confirmado com o retorno de Robert Downey Jr. Desde que Tony Stark nos deixou ao final de Vingadores Ultimato, o Universo Cinematográfico da Marvel nunca mais foi o mesmo. E parece que esta mudança na atmosfera dos filmes foi percebida também pelos grandes executivos da Disney. Como parte da imensa reestruturação que vem sendo feita no Marvel Studios, o playboy filantropo vai retornar aos filmes da Marvel. Em um comunicado ao Deadline, Robert Downey Jr. confirmou que Homem de Ferro 4 está em desenvolvimento. De acordo com Downey Jr., que age como produtor-executivo, o novo longa vai se chamar Homem de Ferro: Guerra das Armaduras. O projeto como um substituto para o roteiro protagonizado por Don Cheadle, originalmente anunciado como uma série do Disney+. Entretanto, a trama deve ser reaproveitada e o Máquina de Combate deve ser mantido como o principal personagem. Na história, James Rhodes estará investigando o sumiço de invenções das Indústrias Stark, mencionadas em Homem-Aranha: Sem Volta Para Casa, e precisará da ajuda de Riri Williams, a Coração de Ferro, quando descobrem que a grande mandante do esquema é uma Inteligência Artificial do Tony Stark chamada JarvisPT, escondida em Wakanda.",
+    "photoCover": "https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/7F51FA9F6CBD9F0C9B1394B1CC0A6A842D07091318674E234CD33CBF7C28CDC3/scale?width=1200&aspectRatio=1.78&format=jpeg"
   },
   {
-    "id":"2",
-    "title": "Nova Série anunciada no Disney +",
-    "description": "bla blabla",
-    "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+    "id": "2",
+    "title": "Novo filme de Capitão América",
+    "description": "Novo filme de Capitão América com Anthony Mackie tem alteração de título. O quarto filme de Capitão América recebeu um novo título após Anthony Mackie, que interpreta Sam Wilson, postou em suas redes sociais, ao lado de Harrison Ford, a mudança. Antes chamado Captain America: New World Order agora será Captain America: Brave New World. A mudança foi provavelmente feita por conta de como New World Order (Nova Ordem Mundial) soar muito antissemita, ainda mais com novos personagens que a Marvel está apresentando e os atuais problemas que a sociedade vem enfrentando. O novo título Brave New World pode ter sido inspirado no livro de 1932 de Aldous Huxley, Admirável Mundo Novo. Contudo, ainda sem tradução oficial, é difícil imaginar como o título do longa deverá ser apresentado no Brasil. Ainda sem uma sinopse oficial, podemos esperar uma dinâmica interessante entre Sam Wilson, agora sob o manto do Capitão, e Thaddeus Thunderbolt Ross, o personagem de Ford. Em uma entrevista à Entertainment Weekly, Kevin Feige expressou sua animação em ver os dois atores atuando juntos: Existe uma dinâmica entre o presidente Ross e Sam Wilson [Mackie]. Eles têm uma história juntos, mas neste filme veremos a dinâmica entre o Capitão América e o presidente dos Estados Unidos de uma maneira incrível. ",
+    "photoCover": "https://assets-prd.ignimgs.com/2022/07/28/falcon-1629313882249-1659040260233.png"
   },
   {
-    "id":"3",
-    "title": "NOVO HOMEM DE FERRO EM 3D",
-    "description": "marvel anuncia um novo filme do homem de ferro, confira",
-    "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/7F51FA9F6CBD9F0C9B1394B1CC0A6A842D07091318674E234CD33CBF7C28CDC3/scale?width=1200&aspectRatio=1.78&format=jpeg"
+    "id": "3",
+    "title": "POLÊMICA MARVEL STUDIOS",
+    "description": "Entenda demissão polêmica de Victoria Alonso, antiga executiva do Marvel Studios. Enquanto a empresa alega quebra de contrato, a executiva se diz silenciada Você já deve ter notado o nome de Victoria Alonso nos créditos dos filmes e séries do Marvel Studios. Afinal de contas, a executiva estava na companhia desde de 2006, primeiro como chefe de efeitos visuais e pós-produção, e depois como produtora-executiva de todas as produções da Casa das Ideias desde Os Vingadores, de 2012. Quer dizer, ela foi parte essencial na consolidação do universo cinematográfico como o conhecemos hoje. Por isso, a notícia da sua demissão, depois de 17 anos na empresa, pegou todos de surpresa. E, conforme mais informações sobre o caso chegam a público, percebe-se que a cisão realmente não foi nada amigável: enquanto um lado alega quebra de contrato, Alonso diz que foi silenciada. Ao menos três fontes da Variety confirmaram que a saída da executiva se deu por causa do seu envolvimento com o longa Argentina, 1985, indicado ao Oscar de Melhor Filme Internacional. Elas dizem que o contrato de Alonso desde 2018 proibia que ela trabalhasse em projetos de qualquer estúdio rival da Disney, empresa-mãe do Marvel Studios. No caso, o filme estrelado por Ricardo Darín é uma coprodução do Amazon Studios e, portanto, seu envolvimento implicaria numa quebra deste acordo.",
+    "photoCover": "https://cdn.ome.lt/iKmPL7in_zLBmMeztP9P1Zmxkbc=/987x0/smart/uploads/conteudo/fotos/victoria-alonso-oscar.jpg"
   },
   {
-    "id":"4",
-    "title": "Nova Série anunciada no Disney +",
-    "description": "bla blabla",
-    "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+    "id": "4",
+    "title": "Novo Filme do doutor estranho",
+    "description": "Doutor Estranho no Multiverso da Loucura | Os principais easter eggs do longa Filme de Sam Raimi incluiu várias referências à história do personagem no cinema e nos gibis Mais novo lançamento do MCU, Doutor Estranho no Multiverso da Loucura pode ter trazido o estilo do diretor Sam Raimi para a franquia, mas o longa segue um dos principais padrões das outras produções do Marvel Studios: ele está lotado de easter eggs.Essas referências começam a ser introduzidas já na primeira cena do longa do filme e só param após a primeira cena pós- créditos. A Marvel avança com a produção de Doutor Estranho 3, que agora pode ter tido a sua data de estreia vazada - saiba mais.",
+    "photoCover": "https://legadodamarvel.com.br/wp-content/uploads/2023/07/Vaza-data-de-estreia-de-Doutor-Estranho-3-proximo-filme-do-heroi-da-Marvel-legadodamarvel.webp"
   },
   {
-    "id":"5",
-    "title": "NOVO HOMEM DE FERRO EM 3D",
-    "description": "marvel anuncia um novo filme do homem de ferro, confira",
-    "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/7F51FA9F6CBD9F0C9B1394B1CC0A6A842D07091318674E234CD33CBF7C28CDC3/scale?width=1200&aspectRatio=1.78&format=jpeg"
+    "id": "5",
+    "title": "NOVO FILME DA SAGA X-MEN",
+    "description": "Revelada a data de estreia de estreia do novo filme dos X-Men, o reboot da Marvel. A Marvel está oficialmente produzindo um novo filme dos X-Men - saiba quando o reboot dos mutantes chegará aos cinemas. Sim, após 20 anos dos X-Men sob controle criativo da Fox, os mutantes irão agora finalmente ganhar um filme produzido pelo Universo Cinematográfico da Marvel, que de acordo com o Deadline já busca por roteiristas. Antes iremos ver o retorno de alguns mutantes em Deadpool 3, mas no caso com os mesmos atores que já conhecemos, como o Hugh Jackman reprisando o papel do Wolverine, Halle Berry reprisando o papel da Tempestade e outros. Após isso haverá um reboot completo, que promete ser muito importante para o futuro da Marvel, com a MTTSH, fonte confiável, dizendo que tem boas chances da próxima saga da Marvel se chamar “Saga Mutante”. E o último filme da saga atual, a Saga do Multiverso, só irá estrear em 2027, sendo no caso Vingadores: Guerras Secretas. Isso significa que a Saga Mutante só terá início após a estreia de Guerras Secretas.       Sendo assim, ficou fácil para os fãs desvendarem quando o próximo filme dos X-Men irá estrear: apenas a partir de 2027, após a estreia de Vingadores: Guerra Secreta, atualmente marcado para mês de maio de 2027. Ainda falta muito.",
+    "photoCover": "https://legadodamarvel.com.br/wp-content/uploads/2023/10/Revelada-a-data-de-estreia-de-estreia-do-novo-filme-dos-X-Men-o-reboot-da-Marvel-legadodamarvel.webp"
   },
   {
-    "id":"6",
-    "title": "Nova Série anunciada no Disney +",
-    "description": "bla blabla",
-    "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+    "id": "6",
+    "title": "TUDO SOBRE VENOM 3",
+    "description": "Após o sucesso de dois filmes, o Venom já está caminhando para sua terceira produção dentro da Sony Pictures, sendo um dos filmes mais aguardados do ano. Venom: A Última Dança, marcado para estrear no dia 24 de outubro, promete ser, como já indica o título, o último filme do anti-herói vivido por Tom Hardy, que também escreve e produz a continuação. O que significa que Kevin Feige, o Presidente da Marvel Studios, terá um pouco de trabalho para realizar o seu novo desejo em Vingadores: Guerras Secretas, que foi revelado pelo jornalista Daniel Richtman. Como os fãs sabem, Vingadores: Guerras Secretas, marcado para 2027, será o fim da Saga do Multiverso, prometendo ter também o maior aproveitamento deste elemento, com muitas surpresas e participações especiais. E agora, além do aguardado retorno do Homem-Aranha do Tobey Maguire, o jornalista Daniel Richtman revelou que a Marvel Studios também pretende contar com a participação do Venom no filme. Será que vão conseguir convencer o Tom Hardy de atuar como Venom mais uma vez? O que acharam? Continue acompanhando o Legado da Marvel para não perder nenhuma novidade!",
+    "photoCover": "https://legadodamarvel.com.br/wp-content/uploads/2024/03/Vingadores-Venom-devera-ter-papel-surpreendente-em-novo-filme-da-equipe-legadodamarvel.webp"
+  },
+  {
+    "id": "7",
+    "title": "NOVO FILME DA SAGA X-MEN",
+    "description": "Revelada a data de estreia de estreia do novo filme dos X-Men, o reboot da Marvel. A Marvel está oficialmente produzindo um novo filme dos X-Men - saiba quando o reboot dos mutantes chegará aos cinemas. Sim, após 20 anos dos X-Men sob controle criativo da Fox, os mutantes irão agora finalmente ganhar um filme produzido pelo Universo Cinematográfico da Marvel, que de acordo com o Deadline já busca por roteiristas. Antes iremos ver o retorno de alguns mutantes em Deadpool 3, mas no caso com os mesmos atores que já conhecemos, como o Hugh Jackman reprisando o papel do Wolverine, Halle Berry reprisando o papel da Tempestade e outros. Após isso haverá um reboot completo, que promete ser muito importante para o futuro da Marvel, com a MTTSH, fonte confiável, dizendo que tem boas chances da próxima saga da Marvel se chamar “Saga Mutante”. E o último filme da saga atual, a Saga do Multiverso, só irá estrear em 2027, sendo no caso Vingadores: Guerras Secretas. Isso significa que a Saga Mutante só terá início após a estreia de Guerras Secretas.       Sendo assim, ficou fácil para os fãs desvendarem quando o próximo filme dos X-Men irá estrear: apenas a partir de 2027, após a estreia de Vingadores: Guerra Secreta, atualmente marcado para mês de maio de 2027. Ainda falta muito.",
+    "photoCover": "https://legadodamarvel.com.br/wp-content/uploads/2023/10/Revelada-a-data-de-estreia-de-estreia-do-novo-filme-dos-X-Men-o-reboot-da-Marvel-legadodamarvel.webp"
+  },
+  {
+    "id": "8",
+    "title": "Nova Filme do doutor estranho",
+    "description": " A Marvel avança com a produção de Doutor Estranho 3, que agora pode ter tido a sua data de estreia vazada - saiba mais.",
+    "photoCover": "https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
   }
+
+
 ]
