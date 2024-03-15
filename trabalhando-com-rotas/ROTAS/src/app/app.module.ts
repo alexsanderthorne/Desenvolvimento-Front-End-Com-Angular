050f212d0434ea005bdd0cac817c949f00a71935
alexsanderthorne <alexsanderthorne@gmail.com>
2024-03-15 15:26:09 -0300

    criando modulos e componentes

---
+++ trabalhando-com-rotas/ROTAS/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, Title } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { IndexModule } from './pages/index/index.module';
+import { PortifolioModule } from './pages/portifolio/portifolio.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    IndexModule,
+    PortifolioModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
