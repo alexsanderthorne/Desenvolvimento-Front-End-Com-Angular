508413de74a5d2f66f07cfd0951d3d579c306962
alexsanderthorne <alexsanderthorne@gmail.com>
2024-03-28 11:33:14 -0300

    criando regras do jogo

--- projeto-final/projeto-buzzfeed/src/app/components/quizz/quizz.component.ts
+++ projeto-final/projeto-buzzfeed/src/app/components/quizz/quizz.component.ts
@@ -1,15 +1,62 @@
 import { Component, OnInit } from '@angular/core';
+import quizz_questions from "../../../assets/data/quizz_questions.json"
 
 @Component({
   selector: 'app-quizz',
   templateUrl: './quizz.component.html',
   styleUrls: ['./quizz.component.css']
 })
+
 export class QuizzComponent implements OnInit {
+
+  title:string = ""
+
+  questions:any
+  questionSelected:any
+
+  answers:string[] = []
+  answerSelected:string =""
+
+  questionIndex:number =0
+  questionMaxIndex:number=0
+
+  finished:boolean = false
 
   constructor() { }
 
   ngOnInit(): void {
+    if(quizz_questions){
+      this.finished = false
+      this.title = quizz_questions.title
+
+      this.questions = quizz_questions.questions
+      this.questionSelected = this.questions[this.questionIndex]
+
+      this.questionIndex = 0
+      this.questionMaxIndex = this.questions.length
+
+      console.log(this.questionIndex)
+      console.log(this.questionMaxIndex)
+    }
+
   }
+
+  playerChoose(value:string){
+    this.answers.push(value)
+    this.nextStep()
+
+  }
+
+  async nextStep(){
+    this.questionIndex+=1
+
+    if(this.questionMaxIndex > this.questionIndex){
+        this.questionSelected = this.questions[this.questionIndex]
+    }else{
+
+    }
+  }
+
+
 
 }
